a16c9a141c9abd95fa945eac2be07fb60245668b
xula <andrerpedruzzi@gmail.com>
2020-03-24 15:49:50 -0300

    Updates

---
+++ precache-manifest.3923cf4b8956658fa7b50e68954e34df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a98f64fc727d68e4ad6e6e6186eaa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8aa858acca4488e6e3aa",
+    "url": "/static/css/main.619254b8.chunk.css"
+  },
+  {
+    "revision": "de177f02896a5fefd68c",
+    "url": "/static/js/2.0ba5ba63.chunk.js"
+  },
+  {
+    "revision": "088762ca49c61ec7ee3934bedd4dacda",
+    "url": "/static/js/2.0ba5ba63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa858acca4488e6e3aa",
+    "url": "/static/js/main.bb7b0858.chunk.js"
+  },
+  {
+    "revision": "4eda1c8fdaa117933956",
+    "url": "/static/js/runtime-main.933f0315.js"
+  },
+  {
+    "revision": "b8f5b770d4ae61ddc8f1bc424d594caa",
+    "url": "/static/media/medsenior.b8f5b770.png"
+  }
+]);
